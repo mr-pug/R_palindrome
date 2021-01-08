a8515a2c752b6913af3aef7e6d6011c5312eb360
Joshua Plymale <joshua@orbitalimpact.com>
2021-01-08 15:29:41 -0500

    bundle it

---
+++ lib/palindrome2.rb
@@ -0,0 +1,6 @@
+require "palindrome2/version"
+
+module Palindrome2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "palindrome2"
+
+require "minitest/autorun"
